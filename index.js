f533562a1bb037d16f90f462309ce20c58dc45b9
phylis13 <phylisnjeri54@gmail.com>
2022-11-28 20:44:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return
+}
+
+function appendCat(name){
+   return  [...cats, name]; 
+}
+
+function prependCat(name){
+    return [name ,...cats];
+}
+
+function removeLastCat(){
+   return cats.slice(0,cats.length-1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
